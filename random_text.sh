ed059f43c610613e6f34abd8027cafb6970504c8
Daniel Proano <179722984+DanielProano@users.noreply.github.com>
2025-07-18 00:06:30 -0400

    More Script practice

---
+++ random_text.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+for i in {1..20}; do
+	head /dev/urandom | tr -dc 'A-Za-z0-9' | head -c 16
+	echo
+done
--- read_data.sh
+++ read_data.sh
@@ -1,6 +1,8 @@
-#! /bin/bash/
+#! /bin/bash
 
 while IFS= read -r line; do
 	IFS=$'\n' read -rd '' -a fields <<< $(tr ',~|-' '\n' <<< "$line")
-	echo $fields
-done < "$1"
+	for word in "${fields[@]}"; do
+		echo "$word" | xargs
+	done
+done < "$1" 
